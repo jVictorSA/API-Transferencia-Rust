a6ce4d14cb52246e6eba018c4e5e858ccdb5def8
Hélder Silva Ferreira Lima <74564010+heldaolima@users.noreply.github.com>
2022-01-05 12:19:47 -0300

    cliente: struct, get, post
    Agora a gente tem a struct Cliente, junto com as funções básicas de GET e POST, necessários nesse momento inicial

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,21 +1,128 @@
-//declaração da dependência
-use actix_web::Responder;
+use async_std::stream::StreamExt;
+use mongodb::bson::doc;
+use serde::{Deserialize, Serialize};
+use tide::{Body, Request, Response, StatusCode};
+use crate::State;
 
-//Funções que representam requisições HTTP.
-//Atualmente só imprimem no terminal quando
-//requisitadas.
-pub async fn get_users() -> impl Responder{
-    format!("hello from users")
+pub async fn hello(_req: Request<State>) -> tide::Result {
+    return Ok("Hello, world!".into()) //into() transforma a str em tide::Result
 }
 
-pub async fn get_users_id() -> impl Responder{
-    format!("hello from get users by id")
+#[derive(Debug, Serialize, Deserialize)]
+// The request's body structure
+pub struct Item {
+  pub name: String,
+  pub price: f32,
 }
 
-pub async fn post_users() -> impl Responder{
-    format!("hello from post users")
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Cliente {
+    pub nome: String,
+    pub num: i32, //id
+    pub cpf: String,
+    pub conta: String,
+    pub saldo: f64,
+    pub pix: String,
 }
 
-pub async fn delete_user() -> impl Responder{
-    format!("hello from delete users")
+pub async fn get_cliente(req: Request<State>) -> tide::Result<tide::Body> {
+    
+    let db = &req.state().db;
+  
+    let clientes_collection = db.collection_with_type::<Cliente>("clientes");
+  
+  
+    let mut cursor = clientes_collection.find(None, None).await?;
+  
+    // Create a new empty Vector of Item
+    let mut data = Vec::<Cliente>::new();
+
+    while let Some(result) = cursor.next().await {
+      if let Ok(cliente) = result {
+        data.push(cliente);
+      }
+    }
+  
+    // Send the response with the list of items
+    return Body::from_json(&data);
 }
+
+pub async fn post_cliente(mut req: Request<State>) -> tide::Result {
+    
+    let cliente = req.body_json::<Cliente>().await?;
+  
+    let db = &req.state().db;
+  
+    let clientes_collection = db.collection_with_type::<Cliente>("clientes");
+  
+    
+    clientes_collection
+      .insert_one(
+        Cliente {
+          nome: cliente.nome,
+          num: cliente.num,
+          cpf: cliente.cpf,
+          conta: cliente.conta,
+          saldo: cliente.saldo,
+          pix: cliente.pix,
+        },
+        None,
+      )
+      .await?;
+  
+    return Ok(Response::new(StatusCode::Ok));
+}
+  
+
+pub async fn post_item(mut req: Request<State>) -> tide::Result {
+  // Read the request's body and transform it into a struct
+  let item = req.body_json::<Item>().await?;
+
+  // Recover the database connection handle from the Tide state
+  let db = &req.state().db;
+
+  // Get a handle to the "items" collection
+  let items_collection = db.collection_with_type::<Item>("items");
+
+  // Insert a new Item in the "items" collection using values
+  // from the request's body
+  items_collection
+    .insert_one(
+      Item {
+        name: item.name,
+        price: item.price,
+      },
+      None,
+    )
+    .await?;
+
+  // Return 200 if everything went fine
+  return Ok(Response::new(StatusCode::Ok));
+}
+
+pub async fn get_items(req: Request<State>) -> tide::Result<tide::Body> {
+    // Recover the database connection handle from the Tide state
+    let db = &req.state().db;
+  
+    // Get a handle to the "items" collection
+    let items_collection = db.collection_with_type::<Item>("items");
+  
+    // Find all the documents from the "items" collection
+    let mut cursor = items_collection.find(None, None).await?;
+  
+    // Create a new empty Vector of Item
+    let mut data = Vec::<Item>::new();
+
+
+    // Loop through the results of the find query
+    while let Some(result) = cursor.next().await {
+      // If the result is ok, add the Item in the Vector
+      if let Ok(item) = result {
+        data.push(item);
+      }
+    }
+  
+    // Send the response with the list of items
+    return Body::from_json(&data);
+  }
+
--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,37 @@
-use async_std::stream::StreamExt;
 use dotenv::dotenv;
-use mongodb::bson::doc;
-use serde::{Deserialize,Serialize};
 use std::env;
-use tide::{Body, Request, Response, StatusCode};
 
-//mod hellos;
+mod handlers;
 
-#[derive(Clone)]
-pub struct State{
+#[derive(Clone, Debug)]
+pub struct State{ //para passar conexão com os controladores do db
     db: mongodb::Database,
 }
 
-pub async fn hello(_req: Request<State>) -> tide::Result{
-    return Ok("Hello world".into());
-}
+#[async_std::main] //isso torna a main assíncrona
+async fn main() -> tide::Result<()> { //tide::result retorna Ok(()) ou um erro
+    dotenv().ok(); //ler o .env e adicionar as variáveis do environment
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct Item{
-    pub name: String,
-    pub price: f32,
-}
-
-#[derive(Deserialize, Serialize)]
-pub struct Cliente{
-    nome: String,
-    conta: i32,
-    saldo: f64,
-    pix: i32,
-}
-
-async fn post_item(mut req: Request<State>) -> tide::Result{
-    let item = req.body_json::<Item>().await?;
-
-    let db = &req.state().db;
-
-    let items_collection = db.collection_with_type::<Item>("items");
-
-    items_collection
-        .insert_one(
-            Item{
-                name: item.name,
-                price: item.price,
-            },
-            None,
-        ).await?;
-
-    return Ok(Response::new(StatusCode::Ok));
-}
-
-async fn get_items(req: Request<State>) -> tide::Result<tide::Body>{
-    let db = &req.state().db;
-
-    let items_collection = db.collection_with_type::<Item>("items");
-
-    let mut cursor = items_collection
-            .find(None, None).await?;
-
-    let mut data = Vec::<Item>::new();
-
-    while let Some(result) = cursor.next().await{
-        if let Ok(item) = result{
-            data.push(item);
-        }
-    }
-
-    return Body::from_json(&data);
-}
-
-
-#[async_std::main]
-async fn main() -> tide::Result<()> {
-    dotenv().ok();
-
-    let mongodb_client_options = 
-        mongodb::options::ClientOptions::parse(&env::var("MONGODB_URI").unwrap()).await?;
-
+    //criar as opções do cliente MongoDB com a string de conexão das variáveis de environment
+    let mongodb_client_options = mongodb::options::ClientOptions::parse(&env::var("MONGODB_URI").unwrap()).await?;
+    
+    //inicializa o cliente mongodb
     let mongodb_client = mongodb::Client::with_options(mongodb_client_options)?;
 
-    let db = mongodb_client.database("rust-api-example");
+    
+    let db = mongodb_client.database("rust-api-exemplo");
+    
 
-    let state = State{db};
+    let state = State {db};
 
     let mut app = tide::with_state(state);
-    app.at("/hello").get(hello);
-    app.at("/items").get(get_items);
-    app.at("/items").post(post_item);
-    app.listen("127.0.0.1:8080").await?;
+
+    app.at("/hello").get(handlers::hello); //endpoint GET/hello. Rode o programa e dê um curl 127.0.0.1:8080/hello: receberás um hello world
+    app.at("/items").get(handlers::get_items);
+    app.at("/items").post(handlers::post_item);
+    app.at("/clientes").get(handlers::get_cliente);
+    app.at("/clientes").post(handlers::post_cliente);
+    app.listen("127.0.0.1:8080").await?; //ouça na porta 8080
 
     return Ok(());
 }
